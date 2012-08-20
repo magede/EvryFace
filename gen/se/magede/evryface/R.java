3bfc7306562d7532268810bc14eb6987f638532e
Magnus Edevag <magnus.edevag@gmail.com>
2012-08-20 16:47:22 +0200

    Updated gui when displaying contact card

--- gen/se/magede/evryface/R.java
+++ gen/se/magede/evryface/R.java
@@ -20,19 +20,22 @@ public final class R {
         public static final int magnus=0x7f020002;
     }
     public static final class id {
-        public static final int TextView01=0x7f0a0009;
-        public static final int editText1=0x7f0a0008;
-        public static final int editText2=0x7f0a000a;
-        public static final int edit_message=0x7f0a0005;
-        public static final int menu_about=0x7f0a000c;
-        public static final int menu_settings=0x7f0a000b;
-        public static final int profileImage=0x7f0a0003;
-        public static final int progressBar1=0x7f0a0006;
-        public static final int textEmail=0x7f0a0001;
-        public static final int textOffice=0x7f0a0002;
-        public static final int textPhone=0x7f0a0000;
-        public static final int textView1=0x7f0a0004;
-        public static final int textView3=0x7f0a0007;
+        public static final int TextView01=0x7f0a000c;
+        public static final int editText1=0x7f0a000b;
+        public static final int editText2=0x7f0a000d;
+        public static final int edit_message=0x7f0a0008;
+        public static final int emailHeading=0x7f0a0002;
+        public static final int menu_about=0x7f0a000f;
+        public static final int menu_settings=0x7f0a000e;
+        public static final int officeHeading=0x7f0a0004;
+        public static final int phoneHeading=0x7f0a0000;
+        public static final int profileImage=0x7f0a0006;
+        public static final int progressBar1=0x7f0a0009;
+        public static final int textEmail=0x7f0a0003;
+        public static final int textOffice=0x7f0a0005;
+        public static final int textPhone=0x7f0a0001;
+        public static final int textView1=0x7f0a0007;
+        public static final int textView3=0x7f0a000a;
     }
     public static final class layout {
         public static final int activity_display_person=0x7f030000;
@@ -51,22 +54,25 @@ public final class R {
         public static final int app_name=0x7f070000;
         public static final int button_send=0x7f070008;
         public static final int edit_message=0x7f070007;
+        public static final int emailHeading=0x7f070010;
         public static final int getButton_activity_main=0x7f070006;
         public static final int hello_world=0x7f070001;
         public static final int infoText_activity_main=0x7f070005;
         public static final int mainInfoText=0x7f07000a;
         public static final int menu_about=0x7f070003;
         public static final int menu_settings=0x7f070002;
+        public static final int officeHeading=0x7f070011;
         public static final int password=0x7f07000e;
-        public static final int pref_host=0x7f070010;
-        public static final int pref_host_default=0x7f070011;
+        public static final int phoneHeading=0x7f07000f;
+        public static final int pref_host=0x7f070013;
+        public static final int pref_host_default=0x7f070014;
         /**  Preferences 
          */
-        public static final int pref_https=0x7f07000f;
-        public static final int pref_password=0x7f070015;
-        public static final int pref_url=0x7f070012;
-        public static final int pref_url_default=0x7f070013;
-        public static final int pref_userName=0x7f070014;
+        public static final int pref_https=0x7f070012;
+        public static final int pref_password=0x7f070018;
+        public static final int pref_url=0x7f070015;
+        public static final int pref_url_default=0x7f070016;
+        public static final int pref_userName=0x7f070017;
         public static final int settingsText=0x7f07000c;
         public static final int title_activity_display_person=0x7f070009;
         public static final int title_activity_main=0x7f070004;
@@ -75,6 +81,9 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f080000;
+        /**  <item name="android:textColor">#33B5E5</item>
+         */
+        public static final int HeaderFont=0x7f080001;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
